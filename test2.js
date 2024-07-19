e8d6bf50b6c812cf83eca8f6f88b2fb2bb8d6c39
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-19 14:34:15 +0000

    String Methods

---
+++ test2.js
@@ -0,0 +1,37 @@
+// 1. charAt Method
+
+// const sentence = "Ashakya he Shakya Kartil Swamiii"
+// // console.log(sentence.charAt(4))
+// index = 8;
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+
+
+// 2. split()
+
+let sent = "How are you?"
+const myArray = sent.split("")
+console.log(myArray)
+
+
+// 3. slice()
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus)
+
+// 4. subStr()
+
+// let text = "Helloworld!";
+// let result = text.substr(0, 6);
+// console.log(result);
+
+// 5. indexOf()
+
+// let text = "Hello world, welcome to the universe.";
+// let result = text.indexOf("w");
+// console.log(result)
+
+// 6. length
+let text = "Hello World!";
+let length = text.length;
+console.log(length);
