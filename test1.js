c96c4bcae7334260b692073b9d51252b2ca7cb6f
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-19 13:39:13 +0000

    string concatenation

---
+++ test1.js
@@ -0,0 +1,17 @@
+//1.  String Concatenation
+
+let name = "tara"
+let number = 12
+
+// console.log(name+number);
+// console.log(name+number+ "value");
+// This is outdated now...
+
+// 2.  backticks - ` ` ..... it is used to concate many words like..my name is tara, i am 19 years old... so 
+// we dont need to Add + sign again and again!!!
+
+// console.log(`my name is tara, i am 19 years old`); 
+
+// suppose u have to use declared variables in this format
+
+console.log(`my name is ${name} and i am ${number} years old `);
