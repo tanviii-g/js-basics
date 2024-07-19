757c5014250796dd27575839b58775000d4cc0cf
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-19 12:28:28 +0000

    DataType Conversion and memory

---
+++ test.js
@@ -0,0 +1,19 @@
+// 1. let score = 33
+// //console.log(typeof score);
+
+// // let name = "tanvi"
+// // console.log(typeof name);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// 2. let Score = "33abc"
+// let valueInNumber = Number(Score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+let Score = null
+ let valueInNumber = Number(Score)
+console.log(valueInNumber)
+ console.log(typeof valueInNumber)
