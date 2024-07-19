50e2d5104cfac01267562e98a4f666dbd070929b
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-19 13:13:57 +0000

    heap and stack

--- test.js
+++ test.js
@@ -13,7 +13,34 @@
 // console.log(valueInNumber)
 // console.log(typeof valueInNumber)
 
-let Score = null
- let valueInNumber = Number(Score)
-console.log(valueInNumber)
- console.log(typeof valueInNumber)
+// let Score = null
+//  let valueInNumber = Number(Score)
+// console.log(valueInNumber)
+//  console.log(typeof valueInNumber)
+
+ // ###################################################################
+
+  // Stack  (primitive) , Heap  (non-primitive)
+
+  // 1. Stack - it gives copy of original value ... 
+
+//   let myName = "tanvi"
+
+//   letAnotherName = myName
+//   AnotherName = "tanviGaikwad"
+
+//   console.log(myName)
+//   console.log(AnotherName)
+
+// 2. Heap - it will give reference of original value...
+
+let userOne = {
+    email: "taani@google.com",
+    upi:  "user@ybl"
+
+}
+
+let userTwo = userOne
+userTwo.email= "tara@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
