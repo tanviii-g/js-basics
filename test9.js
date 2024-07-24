db1a4d4a70448e65e9483dd59a19a1792e5b43c8
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-24 08:52:19 +0000

    scope

---
+++ test9.js
@@ -0,0 +1,12 @@
+// Scope of function
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c); 
+// try to print one by one sentence..
