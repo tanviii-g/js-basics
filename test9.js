1ad65a7ebb8f3e14de8a98c59de9ad31d09f916a
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-24 11:31:19 +0000

    Functions with Scope and different methods to declare and call functions

--- test9.js
+++ test9.js
@@ -1,5 +1,5 @@
 // Scope of function
-
+let a =10
 if(true){
     let a = 10
     const b = 20
@@ -8,5 +8,45 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c); 
-// try to print one by one sentence..
+// console.log( "Inner" , c); 
+// tip : try to print one by one sentence..
+
+
+// nested scope
+function one(){
+    const username = "radha"
+
+    function two(){
+        const website = "insta"
+        console.log(username);
+
+    }
+    // console.log(website); // it is not executed give error bcz.. it is outside the function
+    two()
+}
+one()
+
+
+// interesting example
+// 1. 
+
+console.log(addOne(5)) // here we call the function first and then declare
+function addOne(num){
+    return num+1
+}
+// Output : 6
+
+// 2. example 
+// console.log(addtwo(3))    // here same we call the function
+//const addtwo = function(num2){     // here we create a variable and store inside the function
+    // return num2+2
+// }
+
+//  Error : Cannot access 'addtwo' before initialization....means when we use this method always keep in mind that we cannot call function before its declaration
+
+// 3. example 
+const addThree = function(num3){
+    return num3+3
+}
+console.log(addThree(7))
+// output : 10 ....(7+3)
