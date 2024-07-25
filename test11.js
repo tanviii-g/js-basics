8da4609891728314ce092a21e2e8a1752861d221
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-25 06:13:18 +0000

    IIFE

--- test11.js
+++ test11.js
@@ -1,1 +1,16 @@
 // Immediately Invoked Function Expressions (IIFE)
+
+// sometimes global pollution can make problem, to remove this problem we used IIFE
+// sytax : ()()
+(function database(){
+    // named IIFE
+    console.log("DB Connected");
+}) () ; // here semicolon is imp bcz if we want to run another code 
+
+// same as function but little bit different.. u have to keep in mind the braces 
+
+// this is another example of IIFE by using arrow function...
+(()=> {
+    // unNamed IIFE
+    console.log("Database is connected");
+}) ()
