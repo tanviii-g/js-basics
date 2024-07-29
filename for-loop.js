8013a350a4a231a2fa7879ef97f808e1ff76c2cd
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-29 08:10:23 +0000

    for-loop with break and continue

--- for-loop.js
+++ for-loop.js
@@ -1,6 +1,43 @@
 // for loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    //console.log(element);
+};
+
+for(let i =0; i<=10;i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+
+       // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        
+    }
+
+}
+let myArray = ["yellow", "green","blue"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
     
+}
+
+// break and continue
+
+// for (let index = 0; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`value of i is ${index}`);
+    
+// }
+
+
+for (let index = 0; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`value of i is ${index}`);
 }
