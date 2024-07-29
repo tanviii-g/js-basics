8e6b8f256af3c5e425e3287f471a8db937c98a54
tanviii-g <164235117+tanviii-g@users.noreply.github.com>
2024-07-29 08:31:30 +0000

    while-loop

---
+++ while-loop.js
@@ -0,0 +1,28 @@
+// while
+
+// let index = 0
+// while (index<=10) {
+//     console.log(`value of index is ${index }`);
+//         index = index+2
+
+    
+// }
+
+
+// let myarray = ["red", "blue", "grey"] 
+
+// let arr = 0
+// while (arr < myarray.length) {
+// console.log(` value is ${myarray[arr]}`);
+// arr = arr + 1
+//  }
+
+
+ // do while loop
+
+ let score = 11
+ 
+ do {
+    console.log(`Score is ${score}`);
+score++
+ } while (score <= 10);
